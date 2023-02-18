680238bccf8fe1fc3b4e8a29503e400ad3c6c07c
Filip Gurak <filip.gurak999@gmail.com>
2023-02-18 20:16:09 +0100

    fix @next/font subset error

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,9 @@ import '../styles/globals.css';
 import { Inter } from '@next/font/google';
 import MainHeader from '../components/MainHeader';
 
-const inter = Inter({});
+const inter = Inter({
+  subsets: ['latin'],
+});
 
 export default function RootLayout({
   children,
